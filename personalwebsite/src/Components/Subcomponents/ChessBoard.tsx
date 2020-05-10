4a859489dc55a3c22f7f040029ea1ee763146c88
Jonathan Pesce <jonathanpesce@hotmail.com>
2020-05-10 19:55:13 -0400

    Get the board and the pieces to show on screen

--- personalwebsite/src/Components/Subcomponents/ChessBoard.tsx
+++ personalwebsite/src/Components/Subcomponents/ChessBoard.tsx
@@ -9,6 +9,8 @@ export class ChessBoard extends Component<any, any> {
     }
 
     render() {
-        return <Chessground></Chessground>
+        return (
+            <div className="merida"><Chessground fen={"r1k4r/p2nb1p1/2b4p/1p1n1p2/2PP4/3Q1NB1/1P3PPP/R5K1 b - c3 0 19"}></Chessground></div>
+        );
     }
 }
