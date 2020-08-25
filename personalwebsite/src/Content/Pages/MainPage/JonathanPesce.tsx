a1d547efc4e9475f2c94e6d1adce166822a476cc
Jonathan Pesce <jonathanpesce@hotmail.com>
2020-08-25 09:01:56 -0400

    Add another background image for testing

--- personalwebsite/src/Content/Pages/MainPage/JonathanPesce.tsx
+++ personalwebsite/src/Content/Pages/MainPage/JonathanPesce.tsx
@@ -6,9 +6,10 @@ export function JonathanPesce() {
 
     return (
         <div>
-            <Parallax blur={10} bgImage={'https://raw.githubusercontent.com/PesceJonathan/PersonalWebsite/ChessPage/Assets/JonathanBackground.png'} bgImageAlt="the cat" strength={200}>
-                <div style={{ height: '200px' }} />
+            <Parallax blur={0} bgImage={'https://raw.githubusercontent.com/PesceJonathan/PersonalWebsite/ChessPage/Assets/JonathanBackground.png'} bgImageAlt="the cat" strength={200}>
+                <div style={{ height: '100vh' }}>ffdg</div>
             </Parallax>
+            <div style={{ height: '100vh' }} />
         </div>
     )
 }
