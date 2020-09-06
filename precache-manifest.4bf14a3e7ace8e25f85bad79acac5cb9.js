4f2aab996ff6bc07dcf61e8cb2d3cadd6b592b27
Jonathan Pesce <jonathanpesce@hotmail.com>
2020-09-06 15:21:30 -0400

    Updates

---
+++ precache-manifest.4bf14a3e7ace8e25f85bad79acac5cb9.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06a07559f0c4df9295efc1af75772355",
+    "url": "/PersonalWebsite/index.html"
+  },
+  {
+    "revision": "233e5e9e28ceae36ac81",
+    "url": "/PersonalWebsite/static/css/2.6dde2aa5.chunk.css"
+  },
+  {
+    "revision": "3fac5c6764b4c51bf650",
+    "url": "/PersonalWebsite/static/css/main.b0139113.chunk.css"
+  },
+  {
+    "revision": "233e5e9e28ceae36ac81",
+    "url": "/PersonalWebsite/static/js/2.78cae688.chunk.js"
+  },
+  {
+    "revision": "a8f7c27823482815f523db6d3ff5a88d",
+    "url": "/PersonalWebsite/static/js/2.78cae688.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fac5c6764b4c51bf650",
+    "url": "/PersonalWebsite/static/js/main.2dff2f5a.chunk.js"
+  },
+  {
+    "revision": "05a2b04fe66c16cd332d",
+    "url": "/PersonalWebsite/static/js/runtime-main.b2dd1bbe.js"
+  },
+  {
+    "revision": "68d0f1e74bcf7fb47590df3202a97518",
+    "url": "/PersonalWebsite/static/media/Chart.68d0f1e7.svg"
+  },
+  {
+    "revision": "0ab770a58fdf55e6d6088571fe78a4f6",
+    "url": "/PersonalWebsite/static/media/ChessPawn.0ab770a5.svg"
+  },
+  {
+    "revision": "96578da9ed790fbfb1d80458ec0e8c13",
+    "url": "/PersonalWebsite/static/media/Concordia.96578da9.svg"
+  },
+  {
+    "revision": "360cf29ca8ad45bbc55362cc4839baa1",
+    "url": "/PersonalWebsite/static/media/Dawson.360cf29c.png"
+  },
+  {
+    "revision": "0fdebeee26e2f1e428445fa88c98f339",
+    "url": "/PersonalWebsite/static/media/Fill.0fdebeee.svg"
+  },
+  {
+    "revision": "1fffe6a847dd64e315a51b7a7bf9fab5",
+    "url": "/PersonalWebsite/static/media/IUTDeLyon.1fffe6a8.png"
+  },
+  {
+    "revision": "0e77a9b5cb8a3561d08bd1414113f0f5",
+    "url": "/PersonalWebsite/static/media/Me.0e77a9b5.png"
+  },
+  {
+    "revision": "fde8e8bc5c48a29cf01df1c46e348fae",
+    "url": "/PersonalWebsite/static/media/Plane.fde8e8bc.png"
+  },
+  {
+    "revision": "ace2a019966577c3c8ef46e924f59f2f",
+    "url": "/PersonalWebsite/static/media/RoyalWestAcademy.ace2a019.png"
+  },
+  {
+    "revision": "753dbebca16f4afe63865065dcd433e9",
+    "url": "/PersonalWebsite/static/media/Snake.753dbebc.svg"
+  },
+  {
+    "revision": "17080759c95bceddcaa82a5bb62740cd",
+    "url": "/PersonalWebsite/static/media/Truck.17080759.png"
+  },
+  {
+    "revision": "ac25f0280f6855e5f768670be137984d",
+    "url": "/PersonalWebsite/static/media/WorldMap.ac25f028.svg"
+  },
+  {
+    "revision": "8a6bd56a8d71b44673a36c25e1ca66aa",
+    "url": "/PersonalWebsite/static/media/bB.8a6bd56a.svg"
+  },
+  {
+    "revision": "47af274b8a88eec771681b9ac53dc6d2",
+    "url": "/PersonalWebsite/static/media/bK.47af274b.svg"
+  },
+  {
+    "revision": "0026f14668177f7dd0d4bab167b1a1cf",
+    "url": "/PersonalWebsite/static/media/bN.0026f146.svg"
+  },
+  {
+    "revision": "344e161b311ce904cc0e3be7d7d61090",
+    "url": "/PersonalWebsite/static/media/bP.344e161b.svg"
+  },
+  {
+    "revision": "aed026d0021b7ebfc2164946c0136010",
+    "url": "/PersonalWebsite/static/media/bQ.aed026d0.svg"
+  },
+  {
+    "revision": "027332a07c4f25930a0e527a85965aa4",
+    "url": "/PersonalWebsite/static/media/bR.027332a0.svg"
+  },
+  {
+    "revision": "047cde2a7a20de717516515884251351",
+    "url": "/PersonalWebsite/static/media/background.047cde2a.jpeg"
+  },
+  {
+    "revision": "970550187aa320d378080ae003b365c1",
+    "url": "/PersonalWebsite/static/media/brown.97055018.svg"
+  },
+  {
+    "revision": "66e563de3f6e6fbe7187c1a33c4a9739",
+    "url": "/PersonalWebsite/static/media/cloud.66e563de.svg"
+  },
+  {
+    "revision": "e0eb761a13389f391cf3e6f643597644",
+    "url": "/PersonalWebsite/static/media/wB.e0eb761a.svg"
+  },
+  {
+    "revision": "bb430c603fae05171bfcc6760efda928",
+    "url": "/PersonalWebsite/static/media/wK.bb430c60.svg"
+  },
+  {
+    "revision": "f9c6e522d506c7c3a555e412d34630df",
+    "url": "/PersonalWebsite/static/media/wN.f9c6e522.svg"
+  },
+  {
+    "revision": "4ff1ce7c9541a77b98582a3329e920ed",
+    "url": "/PersonalWebsite/static/media/wP.4ff1ce7c.svg"
+  },
+  {
+    "revision": "4e4432a6b0dfce093177db46dc9bff48",
+    "url": "/PersonalWebsite/static/media/wQ.4e4432a6.svg"
+  },
+  {
+    "revision": "573501233a407695e7b5d5b8e6efc7aa",
+    "url": "/PersonalWebsite/static/media/wR.57350123.svg"
+  },
+  {
+    "revision": "353573fb33da701e8002f2c58cd5fbca",
+    "url": "/PersonalWebsite/static/media/waterBottle.353573fb.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.003aa06ea7cf01f4f32267c2499ec2eb.js"
+  "/PersonalWebsite/precache-manifest.4bf14a3e7ace8e25f85bad79acac5cb9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PersonalWebsite/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
